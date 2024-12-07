649078841dc1d7312e7b4def2603edfd2c555e19
DenGrif <grifon991@gmail.com>
2024-12-07 16:54:38 +0400

    Update_plotly

--- visualizations/plotly_visualizations.py
+++ visualizations/plotly_visualizations.py
@@ -5,11 +5,22 @@ def create_plotly_charts():
     data = pd.read_csv('data/example_data.csv')
 
     # Interactive Bar Chart
-    fig = px.bar(data, x='Category', y='Value', title='Bar Chart (Plotly)', color='Category')
-    fig.write_html('outputs/plotly_bar_chart.html')
+    fig = px.bar(data, x='Category', y='Value', title='Interactive Bar Chart (Интерактивная столбчатая диаграмма)', color='Category')
+    fig.write_html('outputs/plotly_Столбчатая_интерактивная.html')
     fig.show()
 
     # Interactive Line Plot
-    fig = px.line(data, x='Category', y='Value', title='Line Plot (Plotly)', markers=True)
-    fig.write_html('outputs/plotly_line_plot.html')
+    fig = px.line(data, x='Category', y='Value', title='Interactive Line Plot (Интерактивный линейный график)', markers=True)
+    fig.write_html('outputs/plotly_Интерактивный_линейный.html')
+    fig.show()
+
+    # График рассеяния
+    # Создание DataFrame
+    df = pd.DataFrame({
+        "x": [1, 2, 3, 4],
+        "y": [10, 20, 14, 23]
+    })
+
+    fig = px.scatter(df, x='x', y='y', title='График рассеяния')
+    fig.write_html('outputs/plotly_График_рассеяния.html')
     fig.show()
