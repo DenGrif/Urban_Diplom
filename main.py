8f630787f15fe88c8965a47ef35033a718f75a88
DenGrif <grifon991@gmail.com>
2024-12-07 15:41:31 +0400

    Update_seaborn

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from visualizations.seaborn_visualizations import create_seaborn_charts
 from visualizations.plotly_visualizations import create_plotly_charts
 
 if __name__ == "__main__":
-    print("Создаем визуализации с Matplotlib...")
-    create_matplotlib_charts()
+    # print("Создаем визуализации с Matplotlib...")
+    # create_matplotlib_charts()
 
-    # print("Создаем визуализации с Seaborn...")
-    # create_seaborn_charts()
+    print("Создаем визуализации с Seaborn...")
+    create_seaborn_charts()
     #
     # print("Создаем визуализации с Plotly...")
     # create_plotly_charts()
--- visualizations/seaborn_visualizations.py
+++ visualizations/seaborn_visualizations.py
@@ -1,20 +1,30 @@
 import seaborn as sns
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 def create_seaborn_charts():
     data = pd.read_csv('data/example_data.csv')
 
-    # Bar Plot
+    # Bar Plot – столбчатая диаграмма
     plt.figure(figsize=(8, 5))
     sns.barplot(x='Category', y='Value', data=data, palette='pastel')
-    plt.title('Bar Chart (Seaborn)')
-    plt.savefig('outputs/seaborn_bar_chart.png')
+    plt.title('Bar Plot – столбчатая диаграмма')
+    plt.savefig('outputs/seaborn_Столбчатая диаграмма.png')
     plt.show()
 
-    # Scatter Plot
+    # Точечный график (Scatter Plot)
     plt.figure(figsize=(8, 5))
     sns.scatterplot(x='Category', y='Value', data=data, color='blue', s=100)
-    plt.title('Scatter Plot (Seaborn)')
-    plt.savefig('outputs/seaborn_scatter_plot.png')
+    plt.title('Точечный график (Scatter Plot)')
+    plt.savefig('outputs/seaborn_Точечный_график.png')
+    plt.show()
+
+    # Тепловая карта
+    # Создание данных
+    data = np.random.randn(10, 12)
+
+    sns.heatmap(data, annot=True)
+    plt.title('Тепловая карта')
+    plt.savefig('outputs/seaborn_Тепловая_карта.png')
     plt.show()
